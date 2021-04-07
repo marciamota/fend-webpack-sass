807c2c20b30fe78173fff78c1257586eb73b9186
Marcia <marcia.garciam@hotmail.com>
2021-04-06 21:35:28 -0500

    module 5 lesson4.2

--- src/server/index.js
+++ src/server/index.js
@@ -1,12 +1,27 @@
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
+var bodyParser = require('body-parser')
+var cors = require('cors')
+
+var json = {
+    'title': 'test json response',
+    'message': 'this is a message',
+    'time': 'now'
+}
 
 const app = express()
+app.use(cors())
+// to use json
+app.use(bodyParser.json())
+// to use url encoded values
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 
 app.use(express.static('dist'))
 
-console.log(__dirname)
+console.log(JSON.stringify(mockAPIResponse))
 
 app.get('/', function (req, res) {
     res.sendFile('dist/index.html')
@@ -14,7 +29,7 @@ app.get('/', function (req, res) {
 
 app.get('/test', function (req, res) {
     console.log('get requested')
-    res.json(mockAPIResponse)
+    res.json(mockAPIResponse);
 })
 
 // designates what port the app will listen to for incoming requests
